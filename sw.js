22b6cab7eab224bd55f38f769b40a33f9b601671
Zuleyma Pelcastre Vargas <pelcastrevargasz@gmail.com>
2025-10-07 13:01:10 -0600

    0.0.14
    Versiòn catorce

--- sw.js
+++ sw.js
@@ -7,8 +7,8 @@ const urlsToCache = [
     `${BASE_PATH}index.html`,
     `${BASE_PATH}offline.html`,
     `${BASE_PATH}manifest.json`,
-    `${BASE_PATH}./icons/icon-192x192.png`,
-    `${BASE_PATH}./icons/icon-512x512.png`,
+    `${BASE_PATH}./icons/192x192.png`,
+    `${BASE_PATH}./icons/512x512.png`,
 ];
 
 // 2. INSTALLAR -> se ejecuta al instalar un Service Worker (SW)
